4a59693425eec4324bb958c94993f71d4d342300
Leigh Phillips <neurocis@neurocis.me>
2016-06-23 04:22:41 +0000

    Update conf.sample & genesis pub/scriptkeys

--- src/networks.py
+++ src/networks.py
@@ -1,14 +1,9 @@
-# Main network and testnet3 definitions
+# Main network definition
 
 params = {
-    'bitcoin_main': {
-        'pubkey_address': 0,
-        'script_address': 5,
-        'genesis_hash': '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    'piggycoin_main': {
+        'pubkey_address': 118,
+        'script_address': 28,
+        'genesis_hash': '00000561d6f5f76b0c101ba6bac27ad99a18fc8927c6af844adfd913097e9271'
     },
-    'bitcoin_test': {
-        'pubkey_address': 111,
-        'script_address': 196,
-        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
-    }
 }
--- src/utils.py
+++ src/utils.py
@@ -34,8 +34,8 @@ __b58base = len(__b58chars)
 
 global PUBKEY_ADDRESS
 global SCRIPT_ADDRESS
-PUBKEY_ADDRESS = 0
-SCRIPT_ADDRESS = 5
+PUBKEY_ADDRESS = 118
+SCRIPT_ADDRESS = 28
 
 def rev_hex(s):
     return s.decode('hex')[::-1].encode('hex')
@@ -137,7 +137,7 @@ def hash_160_to_script_address(h160):
     return hash_160_to_address(h160, SCRIPT_ADDRESS)
 
 
-def hash_160_to_address(h160, addrtype = 0):
+def hash_160_to_address(h160, addrtype = 118):
     """ Checks if the provided hash is actually 160bits or 20 bytes long and returns the address, else None
     """
     if h160 is None or len(h160) is not 20:
